4e8b5b2901ef0bd96400e0438f188b662f8eaece
Matthias Sohn <matthias.sohn@sap.com>
2024-05-14 18:04:55 +0200

    DynamoDBRefDatabaseIT: Declare thrown checked exception
    Change-Id: Icc47fae4b1b31314eba71a49721e56938681a2c3

--- src/test/java/com/googlesource/gerrit/plugins/validation/dfsrefdb/dynamodb/DynamoDBRefDatabaseIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/validation/dfsrefdb/dynamodb/DynamoDBRefDatabaseIT.java
@@ -20,6 +20,7 @@ import static com.googlesource.gerrit.plugins.validation.dfsrefdb.dynamodb.Confi
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.DYNAMODB;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.gerritforge.gerrit.globalrefdb.GlobalRefDbLockException;
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.TestPlugin;
 import com.google.gerrit.acceptance.WaitUtil;
@@ -129,7 +130,7 @@ public class DynamoDBRefDatabaseIT extends LightweightPluginDaemonTest {
   }
 
   @Test
-  public void isUpToDateShouldReturnFalseWhenRefDoesNotPointToTheStoredRefValue() {
+  public void isUpToDateShouldReturnFalseWhenRefDoesNotPointToTheStoredRefValue() throws GlobalRefDbLockException {
     String refName = "refs/changes/01/01/meta";
     String currentRefValue = "533d3ccf8a650fb26380faa732921a2c74924d5c";
     String previousRefValue = "9f6f2963cf44505428c61b935ff1ca65372cf28c";
@@ -141,7 +142,7 @@ public class DynamoDBRefDatabaseIT extends LightweightPluginDaemonTest {
   }
 
   @Test
-  public void isUpToDateShouldBeConsideredTrueWhenNoPreviousRefExists() {
+  public void isUpToDateShouldBeConsideredTrueWhenNoPreviousRefExists() throws GlobalRefDbLockException {
     String refName = "refs/changes/01/01/meta";
     String currentRefValue = "533d3ccf8a650fb26380faa732921a2c74924d5c";
 
